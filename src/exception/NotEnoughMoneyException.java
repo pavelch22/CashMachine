844deee04e0d265c4dc12de9411f6f66e88a69e2
Pavel <pavel.andreevich.22s@gmail.com>
2016-04-09 21:31:09 +0300

    Added NotEnoughMoneyException

---
+++ src/exception/NotEnoughMoneyException.java
@@ -0,0 +1,8 @@
+package exception;
+
+/**
+ * Thrown when there is not enough money to withdraw or no proper
+ * denominations to get requested amount.
+ */
+public class NotEnoughMoneyException extends Exception {
+}
