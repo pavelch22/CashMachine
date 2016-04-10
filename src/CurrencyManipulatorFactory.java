8349c0483382eb93eb7b23cfff7308a981902dd6
Pavel <pavel.andreevich.22s@gmail.com>
2016-04-10 18:00:44 +0300

    Implemented CurrencyManipulatorFactory class

---
+++ src/CurrencyManipulatorFactory.java
@@ -0,0 +1,38 @@
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Class for work with currency manipulators.
+ */
+public class CurrencyManipulatorFactory {
+    private static final Map<String, CurrencyManipulator> currencyManipulators = new HashMap<>();
+
+    private CurrencyManipulatorFactory() {
+
+    }
+
+    /**
+     * Returns currency manipulator for currency code.
+     *
+     * @param currencyCode currency code
+     * @return currency manipulator associated with currency code
+     */
+    public static CurrencyManipulator getManipulatorByCurrencyCode(String currencyCode) {
+        if (currencyManipulators.containsKey(currencyCode)) {
+            return currencyManipulators.get(currencyCode);
+        }
+        currencyManipulators.put(currencyCode, new CurrencyManipulator(currencyCode));
+        return currencyManipulators.get(currencyCode);
+    }
+
+
+    /**
+     * Returns all currency manipulators.
+     *
+     * @return collection of currency manipulators
+     */
+    public static Collection<CurrencyManipulator> getAllCurrencyManipulators() {
+        return currencyManipulators.values();
+    }
+}
